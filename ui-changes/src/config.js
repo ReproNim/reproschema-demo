3afc58fc0ddaa4cd1b6161e038c92362f9f6db02
yibeichan <yibeichan@gmail.com>
2023-12-11 16:36:28 -0500

    update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
+  githubSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema-demo/main/DemoProtocol/DemoProtocol_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
-  assetsPublicPath: '/demo-protocol/',
+  assetsPublicPath: '/reproschema-demo/',
   backendServer: null
 };
