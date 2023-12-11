c27d6beb305392954a6ce43ca3429e4cda72038f
yibeichan <yibeichan@gmail.com>
2023-12-11 21:47:19 +0000

    Deploying to gh-pages from  @ 0ece26ed33bad812a6a922b27169da4675b62dc2 🚀

--- ui-changes/src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
-  banner: 'This is a demonstration protocol for ReproSchema.',
-  startButton: 'Join',
-  assetsPublicPath: '/demo-protocol/',
-  backendServer: null
-};
